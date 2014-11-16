b7c96cc33411ee888de9922d607d1c0929729c91
Mukund Bhudia <mukund_bhudia@hotmail.com>
2014-11-16 22:42:41 +0000

    Improved reporting of users who have yet to tweet
    Additionally the tweets.js route now logs the actual number of tweets
    found from a query.

--- routes/tweets.js
+++ routes/tweets.js
@@ -19,24 +19,28 @@ router.get('/:screen_name/:tweetCount', function(req, res, next) {
 	if (twitterkeys) {
 		screenName = req.params.screen_name;
 		numberOfTweets = req.params.tweetCount;
-		console.log("Looking for " + numberOfTweets + " tweets from " + screenName + "...");
+		console.log("Looking for " + numberOfTweets + " tweets from @" + screenName + "...");
 
 		twit.get('/statuses/user_timeline.json', {screen_name:screenName, count:numberOfTweets, include_entities:true}, function(data, twitres) {
 
 			tweetData = data;
 		
-			if (tweetData && twitres.statusCode == 200) {
+			if (tweetData && twitres.statusCode == 200 && tweetData.length > 0) {
 				for (var i = 0; i < tweetData.length; i++){
 					tweets.push(tweetData[i]['text']);
 					tweetString += "<li>" + tweetData[i]['text'] + "</li>";
 				}
 
-				console.log("Found " + numberOfTweets + " tweets from " + screenName);
+				console.log("Found " + tweets.length + " tweets from @" + screenName);
 				res.send(tweetString + "</ul>");
+
+			} else if (tweetData.length == 0) {
+				console.log("No tweets found from @" + screenName);
+				res.send("@" + screenName + " has not tweeted yet.");
 
 			} else if (twitres.statusCode == 404) {
 				console.error("No results found");
-				res.send("Opps! There were no tweets found for '" + screenName + "'. Please try again: " + baseUrl);
+				res.send("Opps! There were no tweets found for @" + screenName + ". Please try again: " + baseUrl);
 
 			} else {
 				console.error("Twitter retreival failure");
